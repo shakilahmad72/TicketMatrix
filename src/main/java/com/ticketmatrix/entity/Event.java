6fb168e4051dee970ea48cd0c432f0ee977007e6
shakilahmad72 <iamshakilahmad27@gmail.com>
2026-09-07 05:59:08 +0530

    entity

--- src/main/java/com/ticketmatrix/entity/Event.java
+++ src/main/java/com/ticketmatrix/entity/Event.java
@@ -1,4 +1,26 @@
 package com.ticketmatrix.entity;
 
-public class Event {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "events",
+        indexes = {
+                @Index(name = "idx_event_date", columnList = "event_date"),
+                @Index(name = "idx_event_title", columnList = "title")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event extends BaseEntity{
+
+    @Column(nullable =false, length = 150)
+    private String title;
 }
