cdbeace5370dcbbba5098636184907a551797260
shakilahmad72 <iamshakilahmad27@gmail.com>
2026-09-07 05:50:31 +0530

    entity

---
+++ src/main/java/com/ticketmatrix/entity/BasicEntity.java
@@ -0,0 +1,27 @@
+package com.ticketmatrix.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BasicEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
---
+++ src/main/java/com/ticketmatrix/entity/Reservation.java
@@ -0,0 +1,4 @@
+package com.ticketmatrix.entity;
+
+public class Reservation {
+}
