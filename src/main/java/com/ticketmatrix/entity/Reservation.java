5cfa6d5ae133a8129076f3114527bbe7742b4d34
shakilahmad72 <iamshakilahmad27@gmail.com>
2026-09-10 06:01:39 +0530

    reservationentity

--- src/main/java/com/ticketmatrix/entity/Reservation.java
+++ src/main/java/com/ticketmatrix/entity/Reservation.java
@@ -4,6 +4,8 @@ import com.ticketmatrix.enums.ReservationStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
+
 @Entity
 @Table(
         name = "reservations",
@@ -21,7 +23,7 @@ import lombok.*;
 @Builder
 public class Reservation extends BaseEntity {
 
-    @Column(name = "reservation_token", nullable = flase, unique = true, length = 64)
+    @Column(name = "reservation_token", nullable = false, unique = true, length = 64)
     public String reservationToken; // UUID sent to client
 
     @Column(name = "user_id", nullable = false)
@@ -30,5 +32,14 @@ public class Reservation extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     @Builder.Default
-    private Reservation status = ReservationStatus.PENDING;
+    private ReservationStatus status = ReservationStatus.PENDING;
+
+    @Column(name = "hold_expires_at", nullable = false)
+    private Instant holdExpiresAt; // Typically createdAt + 10 minutes
+
+    @Column(name = "payment_reference_id", length = 100)
+    private String paymentReferenceId; // Stored once payment gateway confirms
+
+    @Column(name = "confirmed_at")
+    private Instant confirmedAt;
 }
--- src/main/java/com/ticketmatrix/repository/ReservationRepository.java
+++ src/main/java/com/ticketmatrix/repository/ReservationRepository.java
@@ -1,5 +1,6 @@
 package com.ticketmatrix.repository;
 
+import com.ticketmatrix.entity.Reservation;
 import com.ticketmatrix.enums.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,6 +9,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
