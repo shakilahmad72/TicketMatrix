56068c403af15f898eec46ec1436be36e469497d
shakilahmad72 <iamshakilahmad27@gmail.com>
2026-09-11 07:56:26 +0530

    dto

--- src/main/java/com/ticketmatrix/dto/request/CreateEventRequest.java
+++ src/main/java/com/ticketmatrix/dto/request/CreateEventRequest.java
@@ -1,4 +1,25 @@
 package com.ticketmatrix.dto.request;
 
-public class CreateEventRequest {
-}
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.Instant;
+
+public record CreateEventRequest (
+
+        @NotBlank(message = "Title cannot be blank")
+        @Size(max = 150, message = "Title cannot exceed 150 characters")
+        String title,
+
+        @Size(max = 1000, message = "Description cannot exceed 1000 characters")
+        String description,
+
+        @NotBlank(message = "Venue cannot be blank")
+        String venue,
+
+        @NotNull(message = "Event date cannot be null")
+        @Future(message = "Event date must be in the future")
+        Instant eventDate
+) {}
